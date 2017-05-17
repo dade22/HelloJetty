f7b45dad670bfaaab7c0d3754b67b398173d5d76
Developer <Developer@10.0.0.92>
2017-05-17 17:51:51 +0200

    added credit on this example (taken from mkyong.com)

--- src/main/java/com/nicelogics/hellojetty/jackson/Jackson2Example.java
+++ src/main/java/com/nicelogics/hellojetty/jackson/Jackson2Example.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Thanks to http://www.mkyong.com/java/jackson-2-convert-java-object-to-from-json/
+ * for this great example
  */
 package com.nicelogics.hellojetty.jackson;
 
