99601e96e201e3a6b301c5bd1818e3aa57dc69f7
davide <dav22@hotmail.it>
2017-06-08 01:57:45 +0200

    Base classes for erp package

---
+++ src/main/java/com/nicelogics/erp/Start.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nicelogics.erp;
+
+import java.awt.Desktop;
+import java.net.URI;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+import com.nicelogics.hellojetty.utility.Logger;
+
+/**
+ *
+ * @author davide
+ */
+public class Start {
+
+    private final static String CLOGNAME = "Start";
+
+    public static void main(String[] args) throws Exception {
+
+        new Start().go(2222);
+    }
+
+    private void go(int port) throws Exception {
+
+        boolean InDev = true;
+        Server server = null;
+
+        try {
+
+            Logger.i(CLOGNAME, "go", "welcome");
+
+            Logger.i(CLOGNAME, "go", "user.dir: " + System.getProperty("user.dir"));
+            Logger.i(CLOGNAME, "go", "user.home: " + System.getProperty("user.home"));
+            Logger.i(CLOGNAME, "go", "java.home: " + System.getProperty("java.home"));
+
+            Logger.i(CLOGNAME, "go", "http://localhost:" + port + "/index.html");
+            Logger.i(CLOGNAME, "go", "\r\n");
+
+            ResourceConfig resourceConfig = new ResourceConfig();
+            resourceConfig.packages("com.nicelogics.erp.rest");
+
+            ServletContainer servletContainer = new ServletContainer(resourceConfig);
+            ServletHolder sh = new ServletHolder(servletContainer);
+            ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+            context.setContextPath("/");
+            context.addServlet(sh, "/*");
+
+            server = new Server(port);
+            server.setHandler(context);
+            server.setStopAtShutdown(true);
+            server.setDumpBeforeStop(true);
+
+            server.start();
+
+            if (InDev && Desktop.isDesktopSupported()) {
+                Desktop.getDesktop().browse(new URI("http://localhost:" + port + "/index.html"));
+            }
+
+            server.join();
+
+        } catch (Exception ex) {
+
+            ex.printStackTrace();
+            throw ex;
+
+        } finally {
+
+            if (server != null) {
+                server.destroy();
+            }
+        }
+
+        Logger.i(CLOGNAME, "go", "bye!");
+    }
+}
---
+++ src/main/java/com/nicelogics/erp/rest/Index.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nicelogics.erp.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("index.html")
+public class Index {
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String hello() {
+
+        int port = 2222;
+        String s = "<html>\r\n";
+
+        s = addSection(s, "/hello");
+        s = addLink(s, "/bye", "http://localhost:" + port + "/hello/bye");
+        s = addLink(s, "/welcome?n=jetty", "http://localhost:" + port + "/hello/welcome?n=jetty");
+
+        s = addSection(s, "/get");
+        s = addLink(s, "/list", "http://localhost:" + port + "/get/list");
+        s = addLink(s, "/student", "http://localhost:" + port + "/get/student");
+
+        s = addSection(s, "/session");
+        s = addLink(s, "/get", "http://localhost:" + port + "/session/get");
+        s = addLink(s, "/set", "http://localhost:" + port + "/session/set?n=jetty");
+        s = addLink(s, "/clear", "http://localhost:" + port + "/session/clear");
+
+        s = addSection(s, "/jackson");
+        s = addLink(s, "/test", "http://localhost:" + port + "/jackson/test");
+
+        s = addSection(s, "/logback");
+        s = addLink(s, "/test", "http://localhost:" + port + "/logback/test");
+
+        s = addSection(s, "/orientdb");
+        s = addLink(s, "/graph", "http://localhost:" + port + "/orientdb/graph");
+        s = addLink(s, "/object", "http://localhost:" + port + "/orientdb/object");
+
+        s += "</html>";
+        return s;
+    }
+
+    private String addLink(String text, String label, String url) {
+        return text + "<a href=\"" + url + "\" target=\"_blank\">" + label + "</a><br>\r\n";
+    }
+
+    private String addSection(String text, String label) {
+        return text + "<h1>" + label + "</h1>\r\n";
+    }
+}
